d8711b7909de862140000597b8046f54a18cc4ed
宋奕 <songyi@MacBook-Pro.local>
2018-07-05 10:44:15 +0800

    Added some pages.(2)

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,10 +1,71 @@
 <!DOCTYPE html>
 <template>
-	<div>
-		<h1>Home</h1>
+	<div class="home container-fluid">
+		<div class="row">
+			<div class="top">
+				<div class="trans">1</div>
+				<div class="triangle">
+					<div class="triangleLeft"></div>
+					<div class="triangleRight"></div>
+					
+				</div>
+				<div class="dot">
+					<div class="dot1"></div>
+					<div class="dot2"></div>
+					<div class="dot3"></div>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
-<script>
-
-</script>
-<style></style>
+<script></script>
+<style>
+	.triangleLeft,.triangleRight{
+		width: 0;
+		height: 0;
+		border-width: 13px;
+		border-style: solid;
+		border-color: transparent #332B05  transparent transparent;
+	}
+	.triangleRight{
+		border-color: transparent transparent transparent #332B05;
+		float: right;
+	}
+	.top{
+		position: fixed;
+		display: flex;
+		flex-wrap: wrap;
+		align-content: space-between;
+		top:0px;
+		left: 0px;
+		background-color: #FEDB42;
+		width: 100%;
+		height: 20%;
+	}
+	.trans {
+		height: 10px;
+	}
+	.triangle{
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		width: 100%;
+		flex-basis: 100%;
+	}
+	.dot {
+		flex-basis: 100%;
+		display: flex;
+		justify-content: center;
+	}
+	.dot1, .dot2, .dot3{
+		width: 10px;
+		height: 10px;
+		border-width: 1px;
+		border-style: solid;
+		border-color: black;
+		border-radius:5px;
+		margin-left: 1%;
+		margin-right: 2%;
+		margin-bottom: 2%;
+	}
+</style>
--- src/components/index.vue
+++ src/components/index.vue
@@ -1,15 +1,35 @@
 <!DOCTYPE html>
 <template>
-	<div>
-		<h1 class="footer">Index</h1>
+	<div class="index container-fluid">
+		<div class="row">
+			<div class="footer">
+				<div class="mainFunction"></div>
+				<div class="mainFunction"></div>
+					<div class="mainFunction"></div>
+				</div>
+		</div>
 		<router-view/>
 	</div>
 </template>
 <script></script>
 <style>
 .footer {
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
 	position: fixed;
 	bottom: 0px;
-	left: 45%;
+	width: 100%;
+	height: 15%;
+	padding-left: 2%;
+	padding-right: 2%;
+	box-shadow:0px -1px 5px lightgray;
 }
+.mainFunction {
+	display: flex;
+	width: 15%;
+	height: 60%;
+	background-color: #FEDB42;
+}
+
 </style>
